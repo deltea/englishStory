7e7dcd0b64ccd15d8c1cd1285ac9b8966a0d595e
thcheetah777 <thcheetah777@gmail.com>
2022-05-02 14:20:22 -0700

    Update data.js

--- data.js
+++ data.js
@@ -5,13 +5,13 @@ Where all the scenes and options are stored at.
 *^*^*^*^*^*^*^*/
 
 const data = {
-  "Forest": {
-    description: "As you awake, you find yourself lying down, looking up at dark treetops, looming over you like a tall figure’s shadow. There are spots of light shining down upon you, glistening through the trees, highlighting the sparkly dew dripping off the ferns surrounding you. You stand up, and look through the trees. On one side of you, you can barely see green grass, of what looks to be a meadow. On the other side, you see a dark, imminent thicket of twisting black vines. You think a while, pondering on which seemed better to walk towards. After all, you had no idea how you got here, all you know is that you came flying through a dark portal, and came crashing down through the treetops.",
-    options: [{location: "Dark Thicket", name: "Dark Thicket"}, {location: "Meadows", name: "Meadows"}]
+  "Tyler's Room": {
+    description: "You wake up to the aroma of chicken and waffles drifting through the house, carried on a flying carpet, made of the wonderful scent of his mother’s specialty and your favorite breakfast, laid out magnificently on the table. You quickly get dressed and rush downstairs. Your mother was already eating her specialty chicken breakfast, You start eating your mother’s delicious and amazeballs breakfast.",
+    options: [{location: "Tell Mother Dream", name: "Tell your mother a dream you had"}]
   },
-  "Dark Thicket": {
-    description: "It's dark.",
-    options: [{location: "Forest", name: "Back"}, {location: "Deeper Dark Thicket", name: "Go deeper into the thicket"}]
+  "Tell Mother Dream": {
+    description: "Yesterday, late in the night, you had a strange dream about a dark sword, resting under a dead tree, under crinkled brown leaves. The sky is dark, and lightning strikes. You squeal in fear, and start rushing through the forest behind you, tripping over vines and branches. You run, faster and faster, until you’re covered in thorns, and scratches are enveloping your body. You run and you run, until you fall through the ground, you scream and try to scramble back out into the world, but it's too late. You fall and you fall and you fall, until you're floating in darkness, nothing to see, nothing to feel, nothing to smell. You tell ur mom the story, and she listens intently.",
+    options: [{location: "Tyler's Room", name: "Back"}]
   },
   "Meadows": {
     description: "It's green. There is a rabbit.",
